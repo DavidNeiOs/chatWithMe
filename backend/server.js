39596379d6090476e51ac386ab544c81f0047140
David <davidneios25@gmail.com>
2018-10-23 15:52:13 -0400

    homepage connection

--- backend/server.js
+++ backend/server.js
@@ -1,139 +1,177 @@
 // DEPENDANCIES
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const socketIO = require('socket.io');
-const sha256 = require('sha256');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const socketIO = require("socket.io");
+const sha256 = require("sha256");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const io = socketIO(server);
 
-app.use(cors(), bodyParser.raw({ type: '*/*'}));
+app.use(cors(), bodyParser.raw({ type: "*/*" }));
 // CONSTANTS
-const MongoUrl = 'mongodb://admin:password1@ds119442.mlab.com:19442/my-database'
-const COOKIE_NAME = 'chatID';
+const MongoUrl =
+  "mongodb://admin:password1@ds119442.mlab.com:19442/my-database";
+const COOKIE_NAME = "chatID";
 
 //----------------------------------------------------
 //                  FUNCTIONS
 //----------------------------------------------------
 function getSession(req) {
-    try {
-        console.log(req.headers)
-        let session = req.headers.cookie != undefined ? req.headers.cookie.split('=')[1].split(';')[0] : '';
-        return session;
-    }
-    catch (err) {
-        console.log('error getting cookie');
-    }
+  try {
+    console.log(req.headers);
+    let session =
+      req.headers.cookie != undefined
+        ? req.headers.cookie.split("=")[1].split(";")[0]
+        : "";
+    return session;
+  } catch (err) {
+    console.log("error getting cookie");
+  }
 }
 //----------------------------------------------------
 //                  END POINTS
 //----------------------------------------------------
-app.get('/session', (req, res) => {
-    // GET INFO FROM FRONT END
-    let currentSession = getSession(req);
-    console.log('session: ' + currentSession);
-    // CALL GETSESSION TO GET THE ID
-    if(currentSession) {
-        MongoClient.connect(MongoUrl, { useNewUrlParser: true }, (err, db) => {
-            if (err) throw err;
-            let dbo = db.db('my-database');
-    
-            //FIND IF SESSION IS ACTIVE
-            dbo.collection('sessions').findOne({token: currentSession}, (err, result) => {
-                if (err) throw err;
-                if(result) {
-                    res.send(JSON.stringify({success: true, result}));
-                }
-            });
-        });
-    } else {
-        res.send(JSON.stringify({success: false}))
-    }
-    
-    // SEND BACK IF USER HAS AN ACTIVE SESSION OR NOT
-})
-
-app.post('/signup', (req, res) => {
-    // GET THE INFO FROM FRONT END
-    let body = JSON.parse(req.body.toString());
-
-    // CHECK THAT THE USER DOESN'T EXIST ALREADY
-    MongoClient.connect(MongoUrl, { useNewUrlParser: true}, (err, db) => {
+app.get("/session", (req, res) => {
+  // GET INFO FROM FRONT END
+  let currentSession = getSession(req);
+  console.log("session: " + currentSession);
+  // CALL GETSESSION TO GET THE ID
+  if (currentSession) {
+    MongoClient.connect(
+      MongoUrl,
+      { useNewUrlParser: true },
+      (err, db) => {
         if (err) throw err;
-        let dbo = db.db('my-database');
-        
-        // IF USER EXISTS SEND ERROR
-        dbo.collection('users').findOne({username: body.username}, function(err, result) {
-            if (err) throw err;
+        let dbo = db.db("my-database");
 
-            if(result) {
-                res.send(JSON.stringify({success: false, message: 'user already exists'}));
-                db.close();
-                return;
-            } else {
-                // CREATE COLLECTION THAT WILL STORE MESSAGES
-                dbo.createCollection(body.username, function (err, res) {
-                    if (err) throw err;
-                    console.log("collection created");
-                });
-
-                // ELSE SAVE THE INFO IN DATABASE
-                dbo.collection('users').insertOne(body, (err, result) => {
-                    if (err) throw err;
-                    // console.log(result.ops[0].username); get username when success
-                    
-                    // CREATE COOKIE
-                    let token = Math.floor(Math.random() * 100000) + '';
-                    // SAVE SESSION IN DB
-                    dbo.collection('sessions').insertOne({token, username: result.ops[0].username}, (err, result)=>{
-                        if (err) throw err;
-                        console.log('session added');
-                    })
-                    res.cookie(COOKIE_NAME, token);
-                    res.send(JSON.stringify({success: true, message: 'user added', username: result.ops[0].username}));
-                    db.close();
-                })
+        //FIND IF SESSION IS ACTIVE
+        dbo
+          .collection("sessions")
+          .findOne({ token: currentSession }, (err, result) => {
+            if (err) {
+              console.log(err);
+              throw err;
             }
-        }); 
-    })   
-})
-app.post('/getMessages', (req, res) => {
-    let messages = [];
-    // GET THE INFO FROM FRONT END
-    let room = JSON.parse(req.body).room;
-    
-    // GET THE ROOM NAME AND SEARCH IN DB
-    MongoClient.connect(MongoUrl, { useNewUrlParser: true }, function (err, db) {
-        if (err) throw err;
-        let dbo = db.db('my-database');
-        
-        dbo.collection(room).find({}).toArray(function (err, result) {
-            if(err) throw err;
-            messages = result;
-            console.log(messages);
+            if (result) {
+              res.send(JSON.stringify({ success: true, result }));
+            }
+          });
+      }
+    );
+  } else {
+    res.send(JSON.stringify({ success: false }));
+  }
+
+  // SEND BACK IF USER HAS AN ACTIVE SESSION OR NOT
+});
+
+app.post("/signup", (req, res) => {
+  // GET THE INFO FROM FRONT END
+  let body = JSON.parse(req.body.toString());
+
+  // CHECK THAT THE USER DOESN'T EXIST ALREADY
+  MongoClient.connect(
+    MongoUrl,
+    { useNewUrlParser: true },
+    (err, db) => {
+      if (err) throw err;
+      let dbo = db.db("my-database");
+
+      // IF USER EXISTS SEND ERROR
+      dbo
+        .collection("users")
+        .findOne({ username: body.username }, function(err, result) {
+          if (err) throw err;
+
+          if (result) {
+            res.send(
+              JSON.stringify({ success: false, message: "user already exists" })
+            );
             db.close();
-            // SEND THE MSGS BACK IN ARRAY FORM
-            res.send(JSON.stringify({status: true, messages}));
-        })
-    })
-})
+            return;
+          } else {
+            // CREATE COLLECTION THAT WILL STORE MESSAGES
+            dbo.createCollection(body.username, function(err, res) {
+              if (err) throw err;
+              console.log("collection created");
+            });
+
+            // ELSE SAVE THE INFO IN DATABASE
+            dbo.collection("users").insertOne(body, (err, result) => {
+              if (err) throw err;
+              // console.log(result.ops[0].username); get username when success
+
+              // CREATE COOKIE
+              let token = Math.floor(Math.random() * 100000) + "";
+              // SAVE SESSION IN DB
+              dbo
+                .collection("sessions")
+                .insertOne(
+                  { token, username: result.ops[0].username },
+                  (err, result) => {
+                    if (err) throw err;
+                    console.log("session added");
+                  }
+                );
+              res.cookie(COOKIE_NAME, token);
+              res.send(
+                JSON.stringify({
+                  success: true,
+                  message: "user added",
+                  username: result.ops[0].username
+                })
+              );
+              db.close();
+            });
+          }
+        });
+    }
+  );
+});
+app.post("/getMessages", (req, res) => {
+  let messages = [];
+  // GET THE INFO FROM FRONT END
+  let room = JSON.parse(req.body).room;
+
+  // GET THE ROOM NAME AND SEARCH IN DB
+  MongoClient.connect(
+    MongoUrl,
+    { useNewUrlParser: true },
+    function(err, db) {
+      if (err) throw err;
+      let dbo = db.db("my-database");
+
+      dbo
+        .collection(room)
+        .find({})
+        .toArray(function(err, result) {
+          if (err) throw err;
+          messages = result;
+          console.log(messages);
+          db.close();
+          // SEND THE MSGS BACK IN ARRAY FORM
+          res.send(JSON.stringify({ status: true, messages }));
+        });
+    }
+  );
+});
 
 //---------------------------------------------------
 //    SOCKET IO
 //---------------------------------------------------
-io.on('connection', socket => {
-    console.log('connected');
-    // ONCE THE CLIENT IS CONNECTED THEY CREATE THE ROOM THEY WILL JOIN
-    socket.on('room', room => {
-        socket.join(room);
-        console.log('room joined ' + room);
-    })
-    // 'END POINT' FOR WHEN USERS SEND CHAT MESSAGES
-    socket.on('chat message', msgObject => {
-        /*io.emit('chat message', msg);
+io.on("connection", socket => {
+  console.log("connected");
+  // ONCE THE CLIENT IS CONNECTED THEY CREATE THE ROOM THEY WILL JOIN
+  socket.on("room", room => {
+    socket.join(room);
+    console.log("room joined " + room);
+  });
+  // 'END POINT' FOR WHEN USERS SEND CHAT MESSAGES
+  socket.on("chat message", msgObject => {
+    /*io.emit('chat message', msg);
         console.log('message: ' + msg);
         MongoClient.connect(MongoUrl, { useNewUrlParser: true }, (err, db) => {
             if (err) throw err;
@@ -144,32 +182,37 @@ io.on('connection', socket => {
                 db.close();
             })
         })*/
-        let message = {user: msgObject.user, message: msgObject.message}
-        io.in(msgObject.room).emit('chat message', message);
-        MongoClient.connect(MongoUrl, { useNewUrlParser: true }, (err, db) => {
-            if (err) throw err;
-            let dbo = db.db('my-database');
-            dbo.collection(msgObject.room).insertOne({ user: msgObject.user, message: msgObject.message },
-                (err, res) => {
-                    if (err) throw err;
-                    console.log('message saved');
-                    db.close();
-                }
-            )
-        })
-    })
-    socket.on('disconnect', () => {
-        console.log('disconnected');
-    })
-})
-
-
+    let message = { user: msgObject.user, message: msgObject.message };
+    io.in(msgObject.room).emit("chat message", message);
+    MongoClient.connect(
+      MongoUrl,
+      { useNewUrlParser: true },
+      (err, db) => {
+        if (err) throw err;
+        let dbo = db.db("my-database");
+        dbo
+          .collection(msgObject.room)
+          .insertOne(
+            { user: msgObject.user, message: msgObject.message },
+            (err, res) => {
+              if (err) throw err;
+              console.log("message saved");
+              db.close();
+            }
+          );
+      }
+    );
+  });
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
 
 // LISTENERS --------------------------------------------
 app.listen(5000, () => {
-    console.log("app listening in port 5000");
-})
+  console.log("app listening in port 5000");
+});
 
 server.listen(4000, () => {
-    console.log("listening in port (io) 4000");
-})
+  console.log("listening in port (io) 4000");
+});
--- frontend/src/components/homepageCmp/homepageCmp.js
+++ frontend/src/components/homepageCmp/homepageCmp.js
@@ -1,62 +1,73 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import './homepageCmp.css';
-
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import "./homepageCmp.css";
 
 class HomepageComponent extends Component {
-    constructor() {
-        super();
-        this.state = {
-            activeSession: undefined,
+  constructor() {
+    super();
+    this.state = {
+      activeSession: 0
+    };
+  }
+  loadChat() {
+    // PUSH THE USERNAME TO THE URL SO CHAT IS RENDERED
+  }
+  componentDidMount() {
+    // FETCH THE INFO FROM SERVER
+    fetch("/session", {
+      method: "GET",
+      mode: "same-origin",
+      credentials: "include"
+    })
+      .then(response => response.text())
+      .then(responseBody => {
+        console.log(responseBody);
+        let ans = JSON.parse(responseBody);
+        // IN RESPONSE SET THE STATE OF ACTIVESESSION
+        if (ans.success) {
+          this.props.history.push("/user/" + ans.result.username);
+          console.log(ans);
+        } else {
+          this.setState({ activeSession: 1 });
         }
-    }
-    loadChat(){
-        // PUSH THE USERNAME TO THE URL SO CHAT IS RENDERED
-    }
-    componentDidMount() {
-        // FETCH THE INFO FROM SERVER
-        fetch('/session', {
-            method: 'GET',
-            mode: 'same-origin',
-            credentials: 'include'
-        })
-          .then(response => response.text())
-          .then(responseBody => {
-              console.log(responseBody);
-              let ans = JSON.parse(responseBody);
-              // IN RESPONSE SET THE STATE OF ACTIVESESSION
-              if(ans.success) {
-                this.props.history.push('/user/' + ans.result.username);
-                console.log(ans);
-              } else {
-                  this.setState({activeSession: false})
-              }
-              
-          })
-       
-    }
-    render() {
-        if(this.state.activeSession === undefined) {
-            return(
-                // before checking the cookie
-                <div className="btns">
-                    <h2 className="loading">LOADING</h2>
-                </div>
-            )
-        } else if (!this.state.activeSession) {
-            return (
-                <div className="btns">
-                    <Link className="signup-btn" to="/signup">
-                        SIGN UP
-                    </Link> 
-                    <Link className="login-btn" to="/login">
-                        LOG IN
-                    </Link>
-                </div>
-            )
-        }
-    }
+      });
+  }
+  render() {
+    return !this.state.activeSession ? (
+      <div className="btns">
+        <h2 className="loading">LOADING</h2>
+      </div>
+    ) : (
+      <div className="btns">
+        <Link className="signup-btn" to="/signup">
+          SIGN UP
+        </Link>
+        <Link className="login-btn" to="/login">
+          LOG IN
+        </Link>
+      </div>
+    );
+    // if (this.state.activeSession === 0) {
+    //   return (
+    //     // before checking the cookie
+    //     <div className="btns">
+    //       <h2 className="loading">LOADING</h2>
+    //     </div>
+    //   );
+    // } else if (this.state.activeSession === 1) {
+    //   return (
+    //     <div className="btns">
+    //       <Link className="signup-btn" to="/signup">
+    //         SIGN UP
+    //       </Link>
+    //       <Link className="login-btn" to="/login">
+    //         LOG IN
+    //       </Link>
+    //     </div>
+    //   );
+    // }
+  }
 }
 
-let homepageComponentWithRouter = withRouter(HomepageComponent)
+let homepageComponentWithRouter = withRouter(HomepageComponent);
 export default homepageComponentWithRouter;
